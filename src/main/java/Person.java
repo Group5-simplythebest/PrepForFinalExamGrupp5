1f14b4e2e2044c73ab18082107b0a7fab55b3a58
hanna <hannaeinarson93@hotmail.com>
2024-10-29 11:04:22 +0100

    Added constructure in person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,6 +5,13 @@ public class Person {
     private String email;
     private String role;
 
+    public Person(String name, int age, String email, String role) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.role = role;
+    }
+
     @Override
     public String toString() {
         return "------USER INFO--------\n" +
