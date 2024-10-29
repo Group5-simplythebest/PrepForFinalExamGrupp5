b02e4fbb2b308d9adf5de80c20a9805bdf56ad83
eckofox1981 <franklineck@pm.me>
2024-10-29 09:42:14 +0100

    worked on Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -3,4 +3,13 @@ public class Person {
     int age;
     String email;
     String role;
+
+    public void printInfo(){
+        System.out.println("------USER INFO--------");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Role: " + role);
+        System.out.println("-----------------------");
+    }
 }
